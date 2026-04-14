eb35f09904e0911a49a2a5f0b5b46b3d0a8dbaaa
helenapolitti <helenapolittirossi@gmail.com>
2026-04-14 16:46:48 -0300

    primeiro commit

---
+++ contagem.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<windows.h>
+void main(){
+	SetConsoleOutputCP(CP_UTF8);
+	char nome[20], sexo, continuar;
+	int cont = 0, m = 0, f = 0;
+	
+	do{
+	
+		printf("Digite seu primeiro nome:\n");
+		scanf(" %s", &nome);
+		printf("Digite seu sexo m/f: \n");
+		scanf(" %c", &sexo);
+		cont++;
+		printf("Você é o paciente número %d\n", cont);
+		if(sexo == 'm') m++;
+		else f++;
+		printf("Mais algum paciente 's' ou 'n': ");
+		scanf(" %c", &continuar);
+	}while(continuar == 's');
+	printf("Hoje foram atendidos %d pacientes,", cont);
+	printf(" %d do sexo masculino e %d do sexo feminino\n", m, f);
+	
+	
+	getch();
+}
